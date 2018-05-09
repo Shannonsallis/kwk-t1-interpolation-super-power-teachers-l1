b92b28d4c094cd11b51a0d0395d7cbf191c7ee5b
Shannonsallis <shannonsallis1@gmail.com>
2018-05-09 19:12:33 +0000

    automatically backed up by learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -5,7 +5,7 @@ end
 
     describe '#display_rainbow' do
     it 'accepts one argument' do
-      
+
   def display_rainbow(aura)
     puts "R: #{aura[0]}, O: #{aura[1]}, Y: #{aura[2]}, G: #{aura[3]}, B: #{aura[4]}, I: #{aura[5]}, V: #{aura[6]}"
   end
